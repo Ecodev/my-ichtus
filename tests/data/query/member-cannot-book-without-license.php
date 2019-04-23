bf3bcde8debd8ab01ae999c44f45c48a152550fd
Sylvain Tissot <sylvain.tissot@ecodev.ch>
2019-04-23 12:40:08 +0200

    Add test API member cannot book without license #6208

---
+++ tests/data/query/member-cannot-book-without-license.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+use Cake\Chronos\Chronos;
+
+return [
+    [
+        'query' => 'mutation ($inputBooking: BookingInput!) {
+            createBooking(input: $inputBooking) {
+                id
+                creator {
+                    id
+                    name
+                }
+                bookable {
+                    id
+                }
+            }
+        }',
+        'variables' => [
+            'inputBooking' => [
+                'startDate' => Chronos::now()->subMinute(1)->toIso8601String(),
+                'bookable' => 3007,
+                'status' => 'booked',
+            ],
+        ],
+    ],
+    [
+        'errors' => [
+            [
+                'message' => 'User "member" with role member is not allowed on resource "Booking#" with privilege "create"',
+                'extensions' => [
+                    'category' => 'Permissions',
+                ],
+                'locations' => [
+                    [
+                        'line' => 2,
+                        'column' => 13,
+                    ],
+                ],
+                'path' => [
+                    'createBooking',
+                ],
+            ],
+        ],
+    ],
+];
