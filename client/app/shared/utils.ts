c5a8951b0432b78cf73c2fddfbe8fdb6b5bf6e3e
Samuel Baptista <samuel.baptista@ecodev.ch>
2019-05-03 01:24:40 +0200

    Fix side effect after first cherry pick xD

--- client/app/shared/utils.ts
+++ client/app/shared/utils.ts
@@ -1,15 +1,3 @@
-import Decimal from 'decimal.js';
-
-export function moneyRoundUp(amount: number): number {
-    return Math.ceil(+Decimal.mul(amount, 100)) / 100;
-}
-
-export function calculateSuggestedPrice(supplierPrice: string, margin: string, vatRate: string): number {
-    const priceWithMargin = +supplierPrice + (+supplierPrice * +margin);
-    const priceWithTaxes = priceWithMargin + priceWithMargin * +vatRate;
-
-    return priceWithTaxes;
-}
 
 /**
  * Copy text to clipboard.
