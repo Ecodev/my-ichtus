47fb92c2f85dd53af2f1fe3a84051553469fe4b0
Sylvain Tissot <sylvain.tissot@ecodev.ch>
2019-05-11 03:38:17 +0200

    Revert "Compute total balance for accounts of type group #6109"
    This reverts commit b759017256ba84345fe03fa679559d5817099ae5.

--- server/Application/Model/Account.php
+++ server/Application/Model/Account.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Application\Model;
 
-use Application\DBAL\Types\AccountTypeType;
 use Application\Traits\HasIban;
 use Application\Traits\HasName;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -123,10 +122,6 @@ class Account extends AbstractModel
      */
     public function getBalance(): string
     {
-        if ($this->type === AccountTypeType::GROUP) {
-            return _em()->getRepository(self::class)->totalBalanceByParent($this);
-        }
-
         return $this->balance;
     }
 
