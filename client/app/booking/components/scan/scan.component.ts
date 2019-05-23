7ac3fafa4054ee4154b0947927091f82543b80ac
Adrien Crivelli <adrien.crivelli@gmail.com>
2019-05-23 16:51:32 +1200

    Backport QR service from emmy #6393

--- client/app/booking/components/scan/scan.component.ts
+++ client/app/booking/components/scan/scan.component.ts
@@ -1,78 +1,51 @@
 import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import jsQR from 'jsqr';
 import { ActivatedRoute, Router } from '@angular/router';
-import { NaturalAlertService } from '@ecodev/natural';
+import { NaturalAbstractController, NaturalAlertService } from '@ecodev/natural';
+import { QrService } from '../../../shared/services/qr.service';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
     selector: 'app-scan',
     templateUrl: './scan.component.html',
     styleUrls: ['./scan.component.scss'],
 })
-export class ScanComponent implements OnInit, OnDestroy {
+export class ScanComponent extends NaturalAbstractController implements OnInit, OnDestroy {
 
     @ViewChild('video') videoRef: ElementRef;
-    @ViewChild('image') imageRef: ElementRef;
-    @ViewChild('canvas') canvasRef: ElementRef;
 
-    private context;
-
-    constructor(public router: Router, private route: ActivatedRoute, private alertService: NaturalAlertService) {
+    constructor(public router: Router,
+                private route: ActivatedRoute,
+                private alertService: NaturalAlertService,
+                private qrService: QrService,
+    ) {
+        super();
     }
 
     ngOnInit() {
-        this.context = this.canvasRef.nativeElement.getContext('2d');
-        this.startScan();
-    }
+        this.qrService.qrCode.pipe(takeUntil(this.ngUnsubscribe)).subscribe(code => {
+            const parsedCode = code.toLowerCase().replace('https://ichtus.club/booking/', '');
+            this.router.navigate(['..', parsedCode], {relativeTo: this.route});
 
-    ngOnDestroy() {
+        }, () => {
+            const message = 'La caméra est indisponible, essaye de taper le code de ton matériel';
+            this.alertService.error(message, 5000);
+            this.router.navigateByUrl('/booking/by-code');
+        });
 
-        if (this.videoRef.nativeElement.srcObject) {
-            this.videoRef.nativeElement.srcObject.getTracks().forEach(track => {
-                track.stop();
-            });
-        }
-    }
-
-    public startScan() {
-        navigator.mediaDevices.getUserMedia({video: {facingMode: 'environment'}}).then(async (stream: MediaStream) => {
+        this.qrService.getStream().subscribe(stream => {
             this.videoRef.nativeElement.srcObject = stream;
             this.videoRef.nativeElement.setAttribute('playsinline', 'true'); // required to tell iOS safari we don't want fullscreen
             this.videoRef.nativeElement.play();
-            requestAnimationFrame(this.tick.bind(this));
-        }).catch((err) => {
-            console.error('Camera inutilisable.', err.name, err.code, err.message);
-            const message = 'La caméra est indisponible, essaye de taper le code de ton matériel';
-            this.alertService.error(message, 7000);
-            setTimeout(() => {
-                this.router.navigateByUrl('/booking/by-code');
-            }, 1000);
         });
+
+        // In case we arrive here by url refresh that avoids to start camera from click on home.component.ts
+        // Won't cause double scanning
+        this.qrService.start();
     }
 
-    tick(): void {
-        if (this.videoRef.nativeElement.readyState === this.videoRef.nativeElement.HAVE_ENOUGH_DATA) {
-            this.canvasRef.nativeElement.height = this.videoRef.nativeElement.videoHeight;
-            this.canvasRef.nativeElement.width = this.videoRef.nativeElement.videoWidth;
-
-            this.context.drawImage(this.videoRef.nativeElement,
-                0,
-                0,
-                this.canvasRef.nativeElement.width,
-                this.canvasRef.nativeElement.height);
-
-            const imgData = this.context.getImageData(0, 0, this.canvasRef.nativeElement.width, this.canvasRef.nativeElement.height);
-            const code = jsQR(imgData.data, imgData.width, imgData.height);
-
-            if (code && code.data) {
-                const parsedCode = code.data.toLowerCase().replace('https://ichtus.club/booking/', '');
-                this.router.navigate(['..', parsedCode], {relativeTo: this.route});
-            } else {
-                requestAnimationFrame(this.tick.bind(this));
-            }
-
-        } else {
-            requestAnimationFrame(this.tick.bind(this));
-        }
+    ngOnDestroy() {
+        super.ngOnDestroy();
+        this.qrService.stop();
     }
 
 }
