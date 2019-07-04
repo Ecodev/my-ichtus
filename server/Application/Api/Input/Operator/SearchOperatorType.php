b68da7c2cb8b96157dcbca3932ba45781642376e
Samuel Baptista <samuel.baptista@ecodev.ch>
2019-07-04 12:28:06 +0200

    Filter bookings by bookable code and show the code in listing #6463

--- server/Application/Api/Input/Operator/SearchOperatorType.php
+++ server/Application/Api/Input/Operator/SearchOperatorType.php
@@ -63,6 +63,7 @@ class SearchOperatorType extends AbstractOperator
             'destination',
             'startComment',
             'endComment ',
+            'code',
         ];
 
         // Find most textual fields for the entity
