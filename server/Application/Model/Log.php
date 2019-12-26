877f0cb603a76df4ebb2adabee6d32faee62603f
Adrien Crivelli <adrien.crivelli@gmail.com>
2019-12-26 15:53:25 +0100

    Unpin dbal

--- server/Application/Model/Log.php
+++ server/Application/Model/Log.php
@@ -13,7 +13,7 @@ use GraphQL\Doctrine\Annotation as API;
  * @ORM\Table(indexes={
  *     @ORM\Index(name="priority", columns={"creation_date"}),
  *     @ORM\Index(name="date_created", columns={"creation_date"}),
- *     @ORM\Index(name="message", columns={"message"})
+ *     @ORM\Index(name="message", columns={"message"}, options={"lengths" = {191}})
  * })
  * @ORM\Entity(repositoryClass="Application\Repository\LogRepository")
  */
