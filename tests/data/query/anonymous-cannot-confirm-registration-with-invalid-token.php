9f4754dff4fbc112b42a694f6e4ab72df98f26cc
Sylvain Tissot <sylvain.tissot@ecodev.ch>
2021-03-24 15:41:17 +0100

    Fix test query result message #6682

--- tests/data/query/anonymous-cannot-confirm-registration-with-invalid-token.php
+++ tests/data/query/anonymous-cannot-confirm-registration-with-invalid-token.php
@@ -24,7 +24,7 @@ return [
     [
         'errors' => [
             [
-                'message' => 'La session a expiré ou le lien n\'est pas valable. Veuillez effectuer une nouvelle demande.',
+                'message' => 'La session a expiré ou le lien n\'est pas valable. Effectue une nouvelle demande.',
                 'extensions' => [
                     'category' => 'Permissions',
                 ],
