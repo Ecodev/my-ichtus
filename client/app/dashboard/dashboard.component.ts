c23d96641eff2cc7b9f6429e3f5125b01a31503e
Adrien Crivelli <adrien.crivelli@gmail.com>
2021-06-01 13:06:11 +0900

    Default admin route is role specific #8155

--- client/app/dashboard/dashboard.component.ts
+++ client/app/dashboard/dashboard.component.ts
@@ -3,6 +3,9 @@ import {UserService} from '../admin/users/services/user.service';
 import {BookingService} from '../admin/bookings/services/booking.service';
 import {ActivatedRoute} from '@angular/router';
 import {PermissionsService} from '../shared/services/permissions.service';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {UserRole} from '../shared/generated-types';
 
 @Component({
     selector: 'app-dashboard',
@@ -11,13 +14,25 @@ import {PermissionsService} from '../shared/services/permissions.service';
 })
 export class DashboardComponent implements OnInit {
     public title = 'my-ichtus';
+    public readonly adminRoute: Observable<string>;
 
     constructor(
         public readonly userService: UserService,
         public readonly bookingService: BookingService,
         public readonly route: ActivatedRoute,
         public readonly permissionsService: PermissionsService,
-    ) {}
+    ) {
+        this.adminRoute = this.route.data.pipe(
+            map(data => {
+                switch (data.viewer.model.role) {
+                    case UserRole.accounting_verificator:
+                        return 'admin/transaction-line';
+                    default:
+                        return 'admin';
+                }
+            }),
+        );
+    }
 
     public ngOnInit(): void {}
 }
