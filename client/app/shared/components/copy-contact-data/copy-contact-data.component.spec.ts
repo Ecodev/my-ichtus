d3f0e0cda1c83f0396b1d3db92c1f35b72bce617
Sylvain Tissot <sylvain.tissot@ecodev.ch>
2021-06-15 12:51:33 +0200

    Fix CopyContactDataComponent test

--- client/app/shared/components/copy-contact-data/copy-contact-data.component.spec.ts
+++ client/app/shared/components/copy-contact-data/copy-contact-data.component.spec.ts
@@ -1,11 +1,13 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
-
+import {BookingsWithOwnerContactVariables, EmailAndPhoneUsersVariables} from '../../generated-types';
 import {CopyContactDataComponent} from './copy-contact-data.component';
 import {MaterialModule} from '../../modules/material.module';
 
 describe('CopyContactDataComponent', () => {
-    let component: CopyContactDataComponent;
-    let fixture: ComponentFixture<CopyContactDataComponent>;
+    let component: CopyContactDataComponent<EmailAndPhoneUsersVariables | BookingsWithOwnerContactVariables>;
+    let fixture: ComponentFixture<
+        CopyContactDataComponent<EmailAndPhoneUsersVariables | BookingsWithOwnerContactVariables>
+    >;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
