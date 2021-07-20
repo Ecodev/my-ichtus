2ab2ffa04dd211fb795bc6c9e021910cb77f2902
Adrien Crivelli <adrien.crivelli@gmail.com>
2021-07-20 12:25:00 +0900

    Avoid intermediate observable for better error flow #8288

--- client/app/admin/configurations/services/configuration.service.ts
+++ client/app/admin/configurations/services/configuration.service.ts
@@ -1,9 +1,14 @@
 import {Apollo} from 'apollo-angular';
 import {NetworkStatus} from '@apollo/client/core';
 import {Injectable} from '@angular/core';
-import {Observable, Subject} from 'rxjs';
-import {filter, map} from 'rxjs/operators';
-import {Configuration, UpdateConfiguration, UpdateConfigurationVariables} from '../../../shared/generated-types';
+import {Observable} from 'rxjs';
+import {filter, map, takeWhile} from 'rxjs/operators';
+import {
+    Configuration,
+    ConfigurationVariables,
+    UpdateConfiguration,
+    UpdateConfigurationVariables,
+} from '../../../shared/generated-types';
 import {configurationQuery, updateConfiguration} from './configuration.queries';
 
 @Injectable({
@@ -13,24 +18,17 @@ export class ConfigurationService {
     constructor(private readonly apollo: Apollo) {}
 
     public get(key: string): Observable<string> {
-        const resultObservable = new Subject<string>();
-
-        const queryRef = this.apollo.watchQuery<Configuration>({
+        const queryRef = this.apollo.watchQuery<Configuration, ConfigurationVariables>({
             query: configurationQuery,
             variables: {key},
             fetchPolicy: 'cache-and-network',
         });
 
-        const subscription = queryRef.valueChanges.pipe(filter(r => !!r.data)).subscribe(result => {
-            const data = result.data['configuration'];
-            resultObservable.next(data);
-            if (result.networkStatus === NetworkStatus.ready) {
-                resultObservable.complete();
-                subscription.unsubscribe();
-            }
-        });
-
-        return resultObservable.asObservable();
+        return queryRef.valueChanges.pipe(
+            filter(result => !!result.data),
+            takeWhile(result => result.networkStatus !== NetworkStatus.ready, true),
+            map(result => result.data.configuration),
+        );
     }
 
     public set(key: string, value: string): Observable<string> {
