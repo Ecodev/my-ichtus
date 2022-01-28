c4db20f6a5d17d683c1fcc0c496ec7c2e1c96862
Adrien Crivelli <adrien.crivelli@gmail.com>
2022-01-28 17:39:51 +0100

    Use modern way to inject things in tests

--- client/app/admin/users/services/user.service.spec.ts
+++ client/app/admin/users/services/user.service.spec.ts
@@ -6,8 +6,13 @@ import {Router} from '@angular/router';
 import {mockApolloProvider} from '../../../shared/testing/MockApolloProvider';
 import {PermissionsService} from '../../../shared/services/permissions.service';
 import {LOCAL_STORAGE, NaturalMemoryStorage} from '@ecodev/natural';
+import {Observable} from 'rxjs';
+import {Permissions_permissions} from 'client/app/shared/generated-types';
 
 describe('UserService', () => {
+    let service: UserService;
+    let apollo: Apollo;
+    let router: Router;
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [RouterTestingModule],
@@ -20,57 +25,54 @@ describe('UserService', () => {
             ],
         });
 
+        service = TestBed.inject(UserService);
+        apollo = TestBed.inject(Apollo);
+        router = TestBed.inject(Router);
+
         // Mock permissions service
-        inject([PermissionsService], (permissionsService: PermissionsService) => {
-            permissionsService.setUser = () => null as any;
-        })();
+        const permissionsService = TestBed.inject(PermissionsService);
+        permissionsService.setUser = () => null as unknown as Observable<Permissions_permissions>;
     });
 
-    it('should get current user', fakeAsync(
-        inject([UserService], (service: UserService) => {
-            let actual: any = null;
-            service.getViewer().subscribe(v => (actual = v));
-            tick(1000);
+    it('should get current user', fakeAsync(() => {
+        let actual: any = null;
+        service.getViewer().subscribe(v => (actual = v));
+        tick(1000);
 
-            expect(actual.__typename).toBe('User');
-        }),
-    ));
+        expect(actual.__typename).toBe('User');
+    }));
 
-    it('should login', fakeAsync(
-        inject([UserService, Apollo], (service: UserService, apollo: Apollo) => {
-            // Spy on resetStore
-            const resetStore = spyOn(apollo.client, 'resetStore').and.callThrough();
+    it('should login', fakeAsync(() => {
+        // Spy on resetStore
+        const resetStore = spyOn(apollo.client, 'resetStore').and.callThrough();
 
-            let actual: any = null;
-            service
-                .login({
-                    login: 'foo',
-                    password: 'bar',
-                })
-                .subscribe(v => (actual = v));
-            tick(1000);
+        let actual: any = null;
+        service
+            .login({
+                login: 'foo',
+                password: 'bar',
+            })
+            .subscribe(v => (actual = v));
+        tick(1000);
 
-            expect(actual.__typename).toBe('User');
-            expect(resetStore).toHaveBeenCalledTimes(1);
-        }),
-    ));
+        expect(actual.__typename).toBe('User');
+        expect(resetStore).toHaveBeenCalledTimes(1);
+    }));
 
-    it('should logout and redirect to /login', fakeAsync(
-        inject([UserService, Apollo, Router], (service: UserService, apollo: Apollo, router: Router) => {
-            // Spy on resetStore
-            const resetStore = spyOn(apollo.client, 'resetStore').and.callThrough();
+    it('should logout and redirect to /login', fakeAsync(() => {
+        // Spy on resetStore
+        const resetStore = spyOn(apollo.client, 'resetStore').and.callThrough();
 
-            // Mock router to do nothing when navigating
-            const navigate = spyOn(router, 'navigate').and.callFake(() => Promise.resolve(true));
+        // Mock router to do nothing when navigating
+        const navigate = spyOn(router, 'navigate').and.callFake(() => Promise.resolve(true));
 
-            let actual: boolean | undefined;
-            service.logout().subscribe(v => (actual = v));
-            tick(1000);
+        let actual: boolean | undefined;
+        service.logout().subscribe(v => (actual = v));
+        tick(1000);
 
-            expect(actual).toBe(true);
-            expect(resetStore).toHaveBeenCalledTimes(1);
-            expect(navigate).toHaveBeenCalledTimes(1);
-            expect(navigate).toHaveBeenCalledWith(['/login'], {queryParams: {logout: true}});
-        }),
-    ));
+        expect(actual).toBe(true);
+        expect(resetStore).toHaveBeenCalledTimes(1);
+        expect(navigate).toHaveBeenCalledTimes(1);
+        expect(navigate).toHaveBeenCalledWith(['/login'], {queryParams: {logout: true}});
+    }));
 });
