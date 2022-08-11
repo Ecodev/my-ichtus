dea40d91bc164d93b127b27b6177ef4cfdb24f2f
Adrien Crivelli <adrien.crivelli@gmail.com>
2022-08-11 10:33:18 +0200

    Move everything under htdocs/

--- live-server.js
+++ live-server.js
@@ -3,13 +3,13 @@ var liveServer = require("live-server");
 var params = {
     port: 8181, // Set the server port. Defaults to 8080.
     host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
-    root: "./app/", // Set root directory that's being served. Defaults to cwd.
+    root: "./htdocs/", // Set root directory that's being served. Defaults to cwd.
     open: true, // When false, it won't load your browser by default.
     ignore: 'scss,my/templates', // comma-separated string for paths to ignore
-    file: "app/index.html", // When set, serve this file for every 404 (useful for single-page applications)
+    file: "htdocs/index.html", // When set, serve this file for every 404 (useful for single-page applications)
     wait: 0, // Waits for all changes, before reloading. Defaults to 0 sec.
     mount: [['/components', './node_modules']], // Mount a directory to a route.
     logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
-    watch: './app/**',
+    watch: './htdocs/**',
   };
 liveServer.start(params);
