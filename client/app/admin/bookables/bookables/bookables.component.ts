a18430ad43441f12e285cde24a52e78d1b3b4b2f
Adrien Crivelli <adrien.crivelli@gmail.com>
2023-02-27 19:00:25 +0700

    Untangle bookable component hierarchy

--- client/app/admin/bookables/bookables/bookables.component.ts
+++ client/app/admin/bookables/bookables/bookables.component.ts
@@ -1,35 +1,27 @@
 import {Component, EventEmitter, Injector, OnInit, Output} from '@angular/core';
-import {NaturalAbstractList} from '@ecodev/natural';
 import {NaturalSearchFacetsService} from '../../../shared/natural-search/natural-search-facets.service';
-import {
-    Bookables_bookables_items,
-    Bookings_bookings_items,
-    CurrentUserForProfile_viewer,
-    UsageBookables_bookables_items,
-} from '../../../shared/generated-types';
+import {Bookables_bookables_items} from '../../../shared/generated-types';
 import {BookableService} from '../services/bookable.service';
 import {PermissionsService} from '../../../shared/services/permissions.service';
-import {UsageBookableService} from '../services/usage-bookable.service';
+import {ParentComponent} from './usage-bookables.component';
+import {BookingService} from '../../bookings/services/booking.service';
 
 @Component({
     selector: 'app-bookables',
     templateUrl: './bookables.component.html',
     styleUrls: ['./bookables.component.scss'],
 })
-export class BookablesComponent extends NaturalAbstractList<BookableService> implements OnInit {
+export class BookablesComponent extends ParentComponent<BookableService> implements OnInit {
     @Output() public readonly bookableClick = new EventEmitter<Bookables_bookables_items>();
-    public readonly hasUsage = false;
-    public pendingApplications: Bookings_bookings_items[] = [];
-
-    public UsageBookableService = UsageBookableService;
 
     public constructor(
         bookableService: BookableService,
         injector: Injector,
         naturalSearchFacetsService: NaturalSearchFacetsService,
         public readonly permissionsService: PermissionsService,
+        bookingService: BookingService,
     ) {
-        super(bookableService, injector);
+        super(bookableService, injector, bookingService);
         this.naturalSearchFacets = naturalSearchFacetsService.get(
             this.route.snapshot.data.isEquipment ? 'equipment' : 'bookables',
         );
@@ -38,13 +30,4 @@ export class BookablesComponent extends NaturalAbstractList<BookableService> imp
     public select(element: Bookables_bookables_items): void {
         this.bookableClick.emit(element);
     }
-
-    /**
-     * placeholder
-     * TODO: remove if we can after finding good solution to duality of BookablesComponent and UsageBookablesComponent
-     */
-    public createApplication(
-        futureOwner: CurrentUserForProfile_viewer,
-        bookable: UsageBookables_bookables_items,
-    ): void {}
 }
--- client/app/admin/bookables/bookables/usage-bookables.component.ts
+++ client/app/admin/bookables/bookables/usage-bookables.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Injector, Input, Output} from '@angular/core';
+import {Component, Directive, EventEmitter, Injector, Input, OnInit, Output} from '@angular/core';
 import {NaturalSearchFacetsService} from '../../../shared/natural-search/natural-search-facets.service';
 import {PermissionsService} from '../../../shared/services/permissions.service';
 import {UsageBookableService} from '../services/usage-bookable.service';
@@ -13,14 +13,36 @@ import {
 import {BookingService} from '../../bookings/services/booking.service';
 import {takeUntil} from 'rxjs/operators';
 import {UserService} from '../../users/services/user.service';
-import {ActivatedRoute} from '@angular/router';
+import {BookableService} from '../services/bookable.service';
+import {ExtractTallOne} from '@ecodev/natural/lib/types/types';
+
+@Directive()
+export class ParentComponent<T extends UsageBookableService | BookableService> extends NaturalAbstractList<T> {
+    public readonly hasUsage: boolean = false;
+    public readonly UsageBookableService = UsageBookableService;
+    public pendingApplications: Bookings_bookings_items[] = [];
+
+    public constructor(service: T, injector: Injector, private readonly bookingService: BookingService) {
+        super(service, injector);
+    }
+
+    /**
+     * Create a bookable application attributed to the owner specified in route.data.futureOwner.
+     */
+    public createApplication(futureOwner: CurrentUserForProfile_viewer, bookable: ExtractTallOne<T>): void {
+        if (futureOwner) {
+            const booking: BookingPartialInput = {status: BookingStatus.application};
+            this.bookingService.createWithBookable(bookable, futureOwner, booking).subscribe();
+        }
+    }
+}
 
 @Component({
     selector: 'app-usage-bookables',
     templateUrl: './bookables.component.html',
     styleUrls: ['./bookables.component.scss'],
 })
-export class UsageBookablesComponent extends NaturalAbstractList<UsageBookableService> {
+export class UsageBookablesComponent extends ParentComponent<UsageBookableService> implements OnInit {
     @Output() public readonly bookableClick = new EventEmitter<UsageBookables_bookables_items>();
 
     @Input()
@@ -31,20 +53,18 @@ export class UsageBookablesComponent extends NaturalAbstractList<UsageBookableSe
         }
     }
 
-    public readonly hasUsage = true;
+    public override readonly hasUsage = true;
     private searchInitialized = false;
-    public pendingApplications: Bookings_bookings_items[] = [];
-    public UsageBookableService = UsageBookableService;
 
     public constructor(
         usageBookableService: UsageBookableService,
         injector: Injector,
         naturalSearchFacetsService: NaturalSearchFacetsService,
         public readonly permissionsService: PermissionsService,
-        private readonly bookingService: BookingService,
         private readonly userService: UserService,
+        bookingService: BookingService,
     ) {
-        super(usageBookableService, injector);
+        super(usageBookableService, injector, bookingService);
 
         if (this.route.snapshot.data.isAdmin) {
             this.naturalSearchFacets = naturalSearchFacetsService.get(
@@ -62,18 +82,5 @@ export class UsageBookablesComponent extends NaturalAbstractList<UsageBookableSe
             .subscribe(result => {
                 this.pendingApplications = result.items;
             });
-    }
-
-    /**
-     * Create a bookable application attributed to the owner specified in route.data.futureOwner.
-     */
-    public createApplication(
-        futureOwner: CurrentUserForProfile_viewer,
-        bookable: UsageBookables_bookables_items,
-    ): void {
-        if (futureOwner) {
-            const booking: BookingPartialInput = {status: BookingStatus.application};
-            this.bookingService.createWithBookable(bookable, futureOwner, booking).subscribe();
-        }
     }
 }
