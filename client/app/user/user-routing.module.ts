45aae6e81aacf248ce23e73fd887b5feb6723489
Adrien Crivelli <adrien.crivelli@gmail.com>
2023-06-12 10:38:35 +0700

    Page title for registering workflow #9652

--- client/app/user/user-routing.module.ts
+++ client/app/user/user-routing.module.ts
@@ -5,11 +5,17 @@ import {RegisterConfirmComponent} from './components/register/register-confirm.c
 import {UserByTokenResolver} from '../admin/users/services/userByToken.resolver';
 import {RequestPasswordResetComponent} from './components/request-password-reset/request-password-reset.component';
 import {ChangePasswordComponent} from './components/change-password/change-password.component';
+import {NaturalSeo} from '@ecodev/natural';
 
 const routes: Routes = [
     {
         path: 'new',
         component: RegisterComponent,
+        data: {
+            seo: {
+                title: `Adhésion`,
+            } satisfies NaturalSeo,
+        },
     },
     {
         path: 'confirm/:token',
@@ -17,16 +23,31 @@ const routes: Routes = [
         resolve: {
             user: UserByTokenResolver,
         },
+        data: {
+            seo: {
+                title: `Confirmation d'adhésion`,
+            } satisfies NaturalSeo,
+        },
     },
     {
         path: 'request-password-reset',
         component: RequestPasswordResetComponent,
+        data: {
+            seo: {
+                title: `Demande de changement de mot de passe`,
+            } satisfies NaturalSeo,
+        },
     },
     {
         path: 'change-password/:token',
         component: ChangePasswordComponent,
         resolve: {
             user: UserByTokenResolver,
+        },
+        data: {
+            seo: {
+                title: `Changement de mot de passe`,
+            } satisfies NaturalSeo,
         },
     },
 ];
