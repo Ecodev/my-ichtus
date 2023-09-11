800dceb5ad3bffdcd67ae08b1756b4c1836a7fbd
Adrien Crivelli <adrien.crivelli@gmail.com>
2023-09-11 18:50:12 +0800

    Navigation tells Vanilla where Vanilla is

--- htdocs/utils/utils.js
+++ htdocs/utils/utils.js
@@ -1,6 +1,6 @@
-function loadJs(filePath, cb) {
+function loadApi(apiServer, filePath, cb) {
     // Create a script tag, set its source
-    var scriptTag = document.createElement('script');
+    const scriptTag = document.createElement('script');
 
     // And listen to it
     if (cb) {
@@ -8,11 +8,11 @@ function loadJs(filePath, cb) {
     }
 
     // Make sure this file actually loads instead of a cached version
-    var cacheBuster = '?time=' + new Date().getTime();
+    const cacheBuster = '?apiServer=' + apiServer + '&time=' + new Date().getTime();
 
     // Set the type of file and where it can be found
     scriptTag.type = 'text/javascript';
-    scriptTag.src = filePath + cacheBuster;
+    scriptTag.src = 'https://' + apiServer + filePath + cacheBuster;
 
     // Finally add it to the <head>
     document.getElementsByTagName('head')[0].appendChild(scriptTag);
