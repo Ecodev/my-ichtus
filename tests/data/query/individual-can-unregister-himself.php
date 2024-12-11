f055ac237dff77976e8266cdbd67ef5f78ad1be0
Adrien Crivelli <adrien.crivelli@gmail.com>
2024-12-11 16:34:21 +0700

    Assert email is sent to admin when unregistering #10967

--- tests/data/query/individual-can-unregister-himself.php
+++ tests/data/query/individual-can-unregister-himself.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+use Application\DBAL\Types\MessageTypeType;
+use Doctrine\DBAL\Connection;
+use PHPUnit\Framework\Assert;
+
 return [
     [
         'query' => 'mutation {
@@ -13,4 +17,16 @@ return [
             'unregister' => true,
         ],
     ],
+    null,
+    function (Connection $connection): void {
+        $count = $connection->executeQuery(
+            'SELECT COUNT(*) FROM message WHERE type = :type AND email = :email',
+            [
+                'type' => MessageTypeType::UNREGISTER,
+                'email' => 'administrator@example.com',
+            ]
+        )->fetchOne();
+
+        Assert::assertSame(1, $count, 'should have sent 1 email to notify admin');
+    },
 ];
