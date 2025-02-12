14ae6df45c877f3a17075b90ab184d0a81040864
Adrien Crivelli <adrien.crivelli@gmail.com>
2025-02-12 18:50:50 +0900

    Report client version to logs.ecodev.ch

--- client/app/shared/services/logger-extra.service.ts
+++ client/app/shared/services/logger-extra.service.ts
@@ -3,6 +3,7 @@ import {NaturalLoggerExtra, NaturalLoggerType} from '@ecodev/natural';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 import {UserService} from '../../admin/users/services/user.service';
+import {localConfig} from '../generated-config';
 
 @Injectable({
     providedIn: 'root',
@@ -13,7 +14,12 @@ export class LoggerExtraService implements NaturalLoggerExtra {
     public getExtras(): Observable<Partial<NaturalLoggerType>> {
         return this.userService.getViewer().pipe(
             map(viewer => {
-                return {viewer: viewer?.login};
+                return {
+                    viewer: viewer?.login,
+                    app: {
+                        version: localConfig.version,
+                    },
+                };
             }),
         );
     }
