1a4b487eeb291c0202e5d2bd52e4781b32e80a74
Samuel Baptista <samuel.baptista@ecodev.ch>
2026-04-24 00:20:32 +0200

    Add canoe-kayk courses #12583

--- client/app/profile/profile.routes.ts
+++ client/app/profile/profile.routes.ts
@@ -140,7 +140,7 @@ export const routes: Routes = [
                     formation('catamaran', '6033'),
                     formation('wing-foil', '6045'),
                     formation('surprise', '6039'),
-                    formation('camps', '6050'),
+                    formation('canoe-kayak', '6049'),
                 ],
             },
         ],
